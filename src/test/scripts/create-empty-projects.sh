17a7a5a7fd236948dfe309ff9139276b3895cdc3
Rafael Oening <rafael.oening@rentcars.com>
2018-06-29 17:12:23 -0300

    Alterada versão do gitlab

--- src/test/scripts/create-empty-projects.sh
+++ src/test/scripts/create-empty-projects.sh
@@ -6,8 +6,8 @@ USER=$4
 PRIVATE_TOKEN=$5
 
 for i in `seq 1 ${NUM_PROJECTS}`; do
-    curl -sSf -X POST --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "http://$GITLAB_SERVER/api/v3/projects" -F "name=$PROJECT_NAME_PREFIX$i" -F 'visibility_level=20' > /dev/null
+    curl -sSf -X POST --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "http://$GITLAB_SERVER/api/v4/projects" -F "name=$PROJECT_NAME_PREFIX$i" -F 'visibility_level=20' > /dev/null
     echo "project $USER/$PROJECT_NAME_PREFIX$i created"
-    curl -sSf -X POST --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "http://$GITLAB_SERVER/api/v3/projects/$USER%2F$PROJECT_NAME_PREFIX$i/repository/files?file_path=Jenkinsfile&branch_name=master&content=echo%20'Hello'&commit_message=create%20a%20Jenkinsfile"  > /dev/null
+    curl -sSf -X POST --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "http://$GITLAB_SERVER/api/v4/projects/$USER%2F$PROJECT_NAME_PREFIX$i/repository/files?file_path=Jenkinsfile&branch_name=master&content=echo%20'Hello'&commit_message=create%20a%20Jenkinsfile"  > /dev/null
     echo "    Jenkinsfile created"
 done
--- src/test/scripts/delete-projects.sh
+++ src/test/scripts/delete-projects.sh
@@ -5,6 +5,6 @@ NUM_PROJECTS=$3
 USER=$4
 PRIVATE_TOKEN=$5
 for i in `seq 1 ${NUM_PROJECTS}`; do
-    echo "http://$GITLAB_SERVER/api/v3/projects/$USER%2F$PROJECT_NAME_PREFIX$i"
-    curl -sSf -X DELETE --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "http://$GITLAB_SERVER/api/v3/projects/$USER%2F$PROJECT_NAME_PREFIX$i" > /dev/null
+    echo "http://$GITLAB_SERVER/api/v4/projects/$USER%2F$PROJECT_NAME_PREFIX$i"
+    curl -sSf -X DELETE --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "http://$GITLAB_SERVER/api/v4/projects/$USER%2F$PROJECT_NAME_PREFIX$i" > /dev/null
 done
